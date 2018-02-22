b5fcfc29fe387d969206012e41606ed8862b863e
Vishwas Hk <Vishwas.HK@mea.ey.net>
2018-02-22 11:57:10 +0530

    Header created

--- photoapp/index.js
+++ photoapp/index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import {
+    AppRegistry
+} from 'react-native';
+import Header from './src/components/header/header';
 
+// Create component
+const App = () => (
+    <Header headerText={'zMusic'} />
+);
+
+//Register component
 AppRegistry.registerComponent('photoapp', () => App);
