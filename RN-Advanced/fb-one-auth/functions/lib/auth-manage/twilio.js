93e53fe529a12b4df60621152fed591c6ef6905d
Vishwas Hk <Vishwas.HK@mea.ey.net>
2018-06-06 12:09:25 +0530

    Lib files removed

--- RN-Advanced/fb-one-auth/functions/lib/auth-manage/twilio.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const twilio = require("twilio");
-const accountSID = 'AC6bf69ada729dc4db408021d84d0fa1d6';
-const authToken = '611e83f110a5a09cfaa9c7f464f6037e';
-exports.Twilio = twilio(accountSID, authToken);
-//# sourceMappingURL=twilio.js.map
