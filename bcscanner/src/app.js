bcfc4caa608c62d61d56cec47415c7b44a68c39c
Vishwas Hk <Vishwas.HK@mea.ey.net>
2018-07-12 12:02:15 +0530

    barcode debuggable

---
+++ bcscanner/src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+  render() {
+    console.log('Barcode')
+    return (
+      <View>
+        <Text>
+          Barcode App
+        </Text>
+      </View>
+    )
+  }
+
+}
+
+export default App;
